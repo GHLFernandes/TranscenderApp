661e815d823fa6ebdeca6f464f2bc448dfdc1df8
Gabriel Fernandes <64507673+GHLFernandes@users.noreply.github.com>
2022-01-20 14:43:29 -0300

    Update api.ts

--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,13 @@
 const ACCESS_TOKEN_MAP_BOX =
-    "pk.eyJ1IjoiZ2FicmllbGY5NiIsImEiOiJja3g5N2Z6MGIwdDM3MnByeW5uNXdyZ2hyIn0.Ui8FEEqY1LCWpi6-iXSWAQ";
+    "ACCESS_TOKEN_MAP_BOX";
+
+export const fetchLocalMapBox = (local: string) =>
+    fetch(
+        `https://api.mapbox.com/geocoding/v5/mapbox.places/${local}.json?${ACCESS_TOKEN_MAP_BOX}`
+    ).then(response => response.json()).then(data => data);
+
+export const fetchUserGithub = (login: string) =>
+    fetch(`https://api.github.com/users/${login}`).then(response => response.json()).then(data => data);";
 
 export const fetchLocalMapBox = (local: string) =>
     fetch(
